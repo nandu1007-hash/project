b351218df02d28ca39504c8a7f98a04780bf39f6
nandu1007-hash <nandakumarloganathan@yahoo.com>
2024-09-16 13:48:42 +0530

    Updated batch size

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from model import Mymodel  # Import your model from the appropriate file
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 LEARNING_RATE = 0.0001
 NUM_EPOCHS = 50
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
